32afe18498c1deb7743a27a1bdb9d218769bc0c4
aaisara12 <aisara121201@gmail.com>
2022-10-28 21:57:35 -0700

    Added subtotal field

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace DinnerCalculator
 {
@@ -23,22 +25,64 @@ namespace DinnerCalculator
     {
         // Note: ObservableCollection is necessary for informing the
         // DataGrid object when a new element has been added so it knows
-        // to prepare another item
+        // to prepare another item (a simple List doesn't have add events)
         internal ObservableCollection<Participant> Participants
         {
             get => participants ?? (participants = new ObservableCollection<Participant>());
             set => participants = value;
         }
         private ObservableCollection<Participant> participants;
+
+        private float subtotal = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             myDataGrid.ItemsSource = Participants;
+            subtotalText.Text = subtotal.ToString();
+
+            Participants.CollectionChanged += SubscribeToNewParticipants;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SubscribeToNewParticipants(object sender, NotifyCollectionChangedEventArgs e)
         {
-            Participants.Add(new Participant { name = "", moneyOwed = 0 });
+            if(e.NewItems != null)
+            {
+                foreach (Participant p in e.NewItems)
+                {
+                    p.OnMoneyOwedChanged += HandleMoneyOwedChanged;
+                }
+            }
+            if(e.OldItems != null)
+            {
+                foreach(Participant p in e.OldItems)
+                {
+                    p.OnMoneyOwedChanged -= HandleMoneyOwedChanged;
+                }
+            }
+            
+        }
+
+        // Wrapper around UpdateTotals to respond to money owed changed event
+        private void HandleMoneyOwedChanged(float newAmt)
+        {
+            UpdateTotals();
+        }
+
+        // Recompute the totals
+        private void UpdateTotals()
+        {
+            subtotal = 0;
+            foreach(Participant p in Participants)
+            {
+                subtotal += p.moneyOwed;
+            }
+            subtotalText.Text = subtotal.ToString();
+        }
+
+        private void AddPerson_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Participants.Add(new Participant { name = "", moneyOwed = 0 }); 
         }
     }
 }
--- Participant.cs
+++ Participant.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace DinnerCalculator
 {
-    internal class Participant 
+    internal class Participant
     {
-        public string name { get; set; }
-        public float moneyOwed { get; set; }
+        public string name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        private string _name = String.Empty;
 
+        public float moneyOwed 
+        {
+            get => _moneyOwed;
+            set
+            {
+                _moneyOwed = value;
+                OnMoneyOwedChanged?.Invoke(_moneyOwed);   
+            }
+        }
+        private float _moneyOwed = 0;
+
+        public event System.Action<float> OnMoneyOwedChanged;
     }
 }
